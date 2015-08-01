799e4275a5fc8b2372f956d72e256673ecd7b22f
Niranjan Rajendran <niranjan94@yahoo.com>
2015-08-01 15:40:34 +0530

    Added aerodynamic coefficients, accelerations and velocities.

--- flightgear-cc-bridge/interface.py
+++ flightgear-cc-bridge/interface.py
@@ -9,13 +9,16 @@ import SimpleHTTPServer
 import SocketServer
 import os
 from datetime import datetime
+import shutil
 
 flightgear_cc_port = 8888
 flightgear_ws_port = 8778
 flightgear_server = "127.0.0.1"
 flightgear_server_port = 5555
+flightgear_process = None
 log = False
 fg = None
+fg_connected = False
 child_process = None
 what_to_log = ""
 log_filename = "log.csv"
@@ -30,8 +33,16 @@ def get(parent, property):
 
 
 def handle_request(what):
+    #
+    # POSITION & GPS
+    #
     if what == "altitude":
         return get("position", "altitude-ft")
+    elif what == "odometer":
+        return get("instrumentation[0]/gps", "odometer")
+    #
+    # ORIENTATION
+    #
     elif what == "pitch":
         return get("orientation", "pitch-deg")
     elif what == "heading":
@@ -62,6 +73,9 @@ def handle_request(what):
         return get("orientation", "p-body")
     elif what == "r-body":
         return get("orientation", "p-body")
+    #
+    # CONTROL SURFACES
+    #
     elif what == "aileron":
         return get("controls[0]/flight", "aileron")
     elif what == "aileron-trim":
@@ -78,6 +92,9 @@ def handle_request(what):
         return get("controls[0]/flight", "flaps")
     elif what == "wing-sweep":
         return get("controls[0]/flight", "wing-sweep")
+    #
+    # VELOCITIES
+    #
     elif what == "vertical-speed":
         return get("velocities", "vertical-speed-fps")
     elif what == "airspeed":
@@ -88,6 +105,36 @@ def handle_request(what):
         return get("velocities", "glideslope")
     elif what == "mach":
         return get("velocities", "mach")
+    elif what == "u":
+        return get("fdm[0]/jsbsim[0]/velocities", "u-fps")
+    elif what == "v":
+        return get("fdm[0]/jsbsim[0]/velocities", "v-fps")
+    elif what == "w":
+        return get("fdm[0]/jsbsim[0]/velocities", "w-fps")
+    elif what == "p":
+        return get("fdm[0]/jsbsim[0]/velocities", "p-rad_sec")
+    elif what == "q":
+        return get("fdm[0]/jsbsim[0]/velocities", "q-rad_sec")
+    elif what == "r":
+        return get("fdm[0]/jsbsim[0]/velocities", "r-rad_sec")
+    #
+    # ACCELERATIONS
+    elif what == "u-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "udot-ft_sec2")
+    elif what == "v-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "vdot-ft_sec2")
+    elif what == "w-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "wdot-ft_sec2")
+    elif what == "p-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "pdot-rad_sec2")
+    elif what == "q-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "qdot-rad_sec2")
+    elif what == "r-dot":
+        return get("fdm[0]/jsbsim[0]/accelerations", "rdot-rad_sec2")
+    #
+    #
+    # ENGINE, THRUST & WEIGHT
+    #
     elif what == "rpm":
         return get("engines[0]/engine", "rpm")
     elif what == "prop-thrust":
@@ -98,24 +145,59 @@ def handle_request(what):
         return get("engines[0]/engine", "torque-ftlb")
     elif what == "fuel-consumed":
         return get("engines[0]/engine", "fuel-consumed-lbs")
-    elif what == "gross-weight":
-        return get("yasim", "gross-weight-lbs")
+    elif what == "weight":
+        return get("fdm[0]/jsbsim[0]/inertia", "weight-lbs")
+    #
+    # AERODYNAMIC COEFFICIENTS
+    #
+    elif what == "CDo":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CDo")
+    elif what == "CDDf":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CDDf")
+    elif what == "CDwbh":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CDwbh")
+    elif what == "CDDe":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CDDe")
+    elif what == "CDbeta":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CDbeta")
+    elif what == "CLwbh":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CLwbh")
+    elif what == "CLDf":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CLDf")
+    elif what == "CLDe":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CLDe")
+    elif what == "CLadot":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CLadot")
+    elif what == "CLq":
+        return get("fdm[0]/jsbsim[0]/aero[0]/coefficient", "CLq")
+    elif what == "cl-squared":
+        return get("fdm[0]/jsbsim[0]/aero", "cl-squared")
+    elif what == "kCDge":
+        return get("fdm[0]/jsbsim[0]/aero[0]/function", "kCDge")
+    elif what == "kCLge":
+        return get("fdm[0]/jsbsim[0]/aero[0]/function", "kCLge")
+    #
+    # DEFAULT - NOT FOUND
+    #
     else:
         return unicode("error:Requested parameter not found")
 
 
 def handle_log(interval, csv):
     global log_filename
-    log_filename = datetime.now().strftime('%Y-%m-%d %H-%M-%S') + ".csv"
-    csv = "".join(csv.split())
-    parameters = csv.split(",")
-    logging_worker(interval, csv, parameters)
-    values = []
-    for what in parameters:
-        values.append(str(handle_request(what)))
-    print "Logging_started" + ",".join(values)
-    return unicode("Parameter logging started.")
 
+    if fg_connected is not None and fg_connected:
+        log_filename = datetime.now().strftime('%Y-%m-%d %H-%M-%S') + ".csv"
+        csv = "".join(csv.split())
+        parameters = csv.split(",")
+        logging_worker(interval, csv, parameters)
+        values = []
+        for what in parameters:
+            values.append(str(handle_request(what)))
+        print "Logging_started" + ",".join(values)
+        return unicode("Parameter logging started.")
+    else:
+        return unicode("error:Please connect to FlightGear before sending commands")
 
 def call_repeatedly(interval, func, *args):
     stopped = Event()
@@ -162,18 +244,30 @@ class SocketHandler(WebSocket):
         global fg
         global child_process
         global flightgear_path
-        fg_connected = fg is not None
-        if self.data == "connect":
+        global fg_connected
+        fg_connected = fg is not None and fg_connected
+        if self.data == "connect fg":
             if not fg_connected:
                 try:
                     fg = FlightGear(flightgear_server, flightgear_server_port)
                     print self.address, 'Connected to FlightGear via telnet on port ' + str(flightgear_server_port)
                     self.sendMessage(unicode("Connected to FlightGear via telnet on port 9000"))
+                    fg_connected = True
                 except:
                     self.sendMessage(unicode("error:Cannot connect to FlightGear. Try the start command."))
             else:
                 self.sendMessage(unicode("Already connected to FlightGear"))
+                fg_connected = True
+
+        elif self.data == "disconnect fg":
+            fg.quit()
+            fg_connected = False
+            print self.address, 'Disconnected from FlightGear '
+            self.sendMessage(unicode("Disconnected from FlightGear"))
+
         elif self.data == "exit":
+            http_process.terminate()
+            ws_process.terminate()
             sys.exit()
 
         elif self.data.startswith("set_path:"):
@@ -181,7 +275,8 @@ class SocketHandler(WebSocket):
             if input_cmd_str is not None:
                 if input_cmd_str != "":
                     flightgear_path = input_cmd_str
-
+            os.remove(flightgear_path + "\\data\\Aircraft\\c172p\\splash.png")
+            shutil.copy("splash.png", flightgear_path + "\\data\\Aircraft\\c172p\\")
             self.sendMessage(unicode("FlightGear path has been set."))
 
         elif self.data == "start fg":
@@ -189,21 +284,23 @@ class SocketHandler(WebSocket):
             subprocess.Popen([flightgear_path + '\\bin\\fgfs.exe',
                               '--fg-root=' + flightgear_path + '\\data',
                               '--fg-scenery=' + flightgear_path + '\\data\\Scenery',
-                              '--aircraft=Cub',
+                              '--aircraft=c172p',
                               '--disable-random-objects',
                               '--prop:/sim/rendering/random-vegetation=false',
                               '--disable-ai-models',
                               '--disable-ai-traffic',
                               '--disable-real-weather-fetch',
-                              '--geometry=800x600',
+                              '--geometry=800x600',  # DISPLAY RESOLUTION
+                              '--model-hz=120'  # FREQUENCY OF THE FDM
                               '--bpp=32',
-                              '--fdm=yasim',
-                              '--wind=0@0',
+                              '--wind=0@0',  # WIND SPEED AND DIRECTION
+                              '--fog-fastest',
                               '--turbulence=0',
                               '--disable-terrasync',
                               '--timeofday=noon',
                               '--disable-fgcom',
-                              '--telnet=socket,out,30,localhost,5555,udp'])
+                              '--telnet=socket,out,60,localhost,5555,udp'  # TELNET SERVER CONFIGURATION
+                              ])
 
             self.sendMessage(unicode("FlightGear has been started."))
         elif self.data.startswith("log:"):
