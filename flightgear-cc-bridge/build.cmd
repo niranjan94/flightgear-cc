3057c5881abd7e22622003da24b3609c7acfb75f
Niranjan Rajendran <niranjan94@yahoo.com>
2015-08-02 14:41:51 +0530

    Modified build script to create a zip file

--- flightgear-cc-bridge/build.cmd
+++ flightgear-cc-bridge/build.cmd
@@ -2,7 +2,10 @@ rmdir /Q /S dist
 python setup.py py2exe
 mkdir dist\ui
 xcopy G:\web\flightgear-cc-interface dist\ui /s /h
-cp splash.png dist
-cp README.txt dist
+cp extras\splash.png dist
+cp extras\README.txt dist
+cp extras\favicon.ico dist\ui
 rmdir /Q /S dist\ui\.idea
 rmdir /Q /S build
+cd dist
+"C:\Program Files\7-Zip\7za.exe" a -mx9 -tzip FlightGearCC_v.zip *.* -r
